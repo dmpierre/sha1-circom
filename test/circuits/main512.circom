ad1f0b7e30c9394eafedc81fd9a70a2ebe6318a5
Pierre DM <pdaixmoreux@gmail.com>
2023-01-30 09:26:10 +0100

    add 512bits test

---
+++ test/circuits/main512.circom
@@ -0,0 +1,5 @@
+pragma circom 2.0.3;
+
+include "../../circuits/sha1.circom";
+
+component main = Sha1(512);
--- test/sha1.ts
+++ test/sha1.ts
@@ -8,7 +8,7 @@ import { expect } from "chai";
 describe("Computing SHA-1", () => {
   it("Should output correct hash value of a 24bits input", async () => {
     
-    const cir = await wasm_tester(path.join(__dirname, "../../test/circuits/main.circom"));
+    const cir = await wasm_tester(path.join(__dirname, "../../test/circuits/main24.circom"));
 
     const testStr = "abc";
     const b = Buffer.from(testStr, "utf-8");
@@ -29,4 +29,26 @@ describe("Computing SHA-1", () => {
 
     expect(hash).equal(formattedHashOut);
   });
+  it("Should output correct hash value of 512bits inputs", async () => {
+    const cir = await wasm_tester(path.join(__dirname, "../../test/circuits/main512.circom"));
+    const testStr = "abcabcababcabcababcabcababcabcababcabcababcabcababcabcababcabcab";
+    const b = Buffer.from(testStr, "utf-8");
+
+    const hash = crypto.createHash("sha1").update(b).digest("hex");
+
+    const arrIn = buffer2bitArray(b);
+    const witness = await cir.calculateWitness({ in: arrIn }, true);
+    const arrOut = witness.slice(1, 161);
+
+    const h0 = bitArrayToHex(arrOut.slice(0, 32).reverse());
+    const h1 = bitArrayToHex(arrOut.slice(32, 64).reverse());
+    const h2 = bitArrayToHex(arrOut.slice(64, 96).reverse());
+    const h3 = bitArrayToHex(arrOut.slice(96, 128).reverse());
+    const h4 = bitArrayToHex(arrOut.slice(128, 160).reverse());
+
+    const formattedHashOut = `${h0}${h1}${h2}${h3}${h4}`;
+
+    expect(hash).equal(formattedHashOut);
+    
+  })
 });
